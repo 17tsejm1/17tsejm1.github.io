72e99c68242ba5e81d7636e1a5c1b7dadd9ac729
17tsejm1 <17tsejm1@stconline.edu.hk>
2015-03-06 09:32:29 +0800

    Removal of all files
    Let’s start fresh

--- Sha_Tin_College_Dragons/Dragon/Entries/2014/9/23_Mervyns_"Speech"_files/23_Mervyns_"Speech".js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20141126
-
-setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{dynamicallyPopulate();loadMozillaCSS('23_Mervyns_"Speech"_files/23_Mervyns_"Speech"Moz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
