c5ef701c4bbbb57fd74e9ba7314d62c3c96bd443
17tsejm1 <17tsejm1@stconline.edu.hk>
2015-03-19 12:54:45 +0800

    Complete Site Redesign
    Will be adding back features missing from this release soon.

--- Official STC Dragon Site/Sha_Tin_College_Dragons/Dragon/Entries/2014/9/24_Swimming_Gala_files/24_Swimming_Gala.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20141126
-
-setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{dynamicallyPopulate();loadMozillaCSS('24_Swimming_Gala_files/24_Swimming_GalaMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
