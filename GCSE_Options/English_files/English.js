de86a9c9f5652490fce472bc5cbb35988a482b71
17tsejm1 <17tsejm1@stconline.edu.hk>
2015-03-24 22:40:35 +0800

    GCSE Site Update #1
    Added Core Subject Information

---
+++ GCSE_Options/English_files/English.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20150324
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('English_files/EnglishMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
--- GCSE_Options/GTS_files/GTS.js
+++ GCSE_Options/GTS_files/GTS.js
@@ -3,7 +3,7 @@
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('GCSE_English_files/GCSE_EnglishMoz.css')
+{loadMozillaCSS('GTS_files/GTSMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
