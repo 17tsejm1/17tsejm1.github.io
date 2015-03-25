2c1c3a183bd82629675a2d605197183b37d3fb12
17tsejm1 <17tsejm1@stconline.edu.hk>
2015-03-25 12:38:52 +0800

    Update to GCSE instead of GCSE_Options
    Update

---
+++ GCSE/Physical_Education_files/Physical_Education.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20150324
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Physical_Education_files/Physical_EducationMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
