5eb4a68bc1b3e58a326cff886601476a4cfb5fa1
Jordan Tse <jordantse@mac.com>
2015-05-26 09:15:04 +0800

    Updated Air Pollution Site & Genocide Memorial Site
    Updated Air Pollution Site & Genocide Memorial Site

---
+++ Pop Up Game.js
@@ -0,0 +1,37 @@
+var myNumber = 4;
+alert("My number is " + myNumber)
+myNumber = 2;
+alert("Actually, my number now is "+ myNumber);
+var yourNumber = prompt("Give me a number!");
+alert("I have " + myNumber + "and you have " + yourNumber);
+alert("Together, we have " + (myNumber + yourNumber));
+alert("Oops, I mean we have " + (myNumber + + yourNumber));
+
+if (score > 100) { // add bonus
+	// bonus code here
+	}else{
+	// no bonus;
+	}
+
+while (Player is alive) {
+	// Score increases
+	}
+
+do {
+	// Game Continues
+	} while (Player above bottom)
+
+for (var lives = 3, lives > 0, lives--) {
+	//Game plays
+	}
+
+
+
+	function calcSum() {
+      value1 =
+        parseInt(document.mainForm.textBox1.value);
+      value2 =
+        parseInt(document.mainForm.textBox2.value);
+      sum = value1 + value2;
+      document.mainForm.textBoxSum.value = sum;
+    }
